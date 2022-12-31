011650da11f7df710d247e0773d711dc0dd9b242
zmjaction <748173631@qq.com>
2022-12-31 22:40:48 +0800

    feat: second commit

---
+++ cmd/zblog/main.go
@@ -0,0 +1,13 @@
+// Copyright 2022 Innkeeper zmjaction(zhaomingjun) <748173631@qq.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/marmotedu/miniblog.
+
+package main
+
+import "fmt"
+
+// Go 程序的默认入口函数(主函数).
+func main() {
+	fmt.Println("Hello ZBlog!")
+}
